166d73627f937dc437ca99e7cce3a4b1ad3bc74c
linxuhan <768445037@qq.com>
2020-09-22 23:24:50 +0800

    首页显示

--- src/components/common/swiper/Swiper.vue
+++ src/components/common/swiper/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="swiper">
-		<!-- <div class="swiper" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd"> -->
 		<div class="swiper" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd">
+		<!-- <div class="swiper"> -->
 			<slot></slot>
 		</div>
 		<slot name="indicator"></slot>
@@ -63,9 +63,27 @@
 			scrollContent(currentPosition) {
 				this.scrolling = true
 				this.swiperStyle.transition = 'transform ' + this.animDuration + 'ms'
-				// this.setTransform(currentPosition)
-				// this.checkPosition()
+				this.setTransform(currentPosition)
+				this.checkPosition()
 				this.scrolling = false
+			},
+			checkPosition() {
+				window.setTimeout(() => {
+					this.swiperStyle.transition = '0ms'
+					if (this.currentIndex >= this.slideCount + 1) {
+						this.currentIndex = 1
+						this.setTransform(-this.currentIndex * this.totalWidth)
+					} else if (this.slideCount <= 0) {
+						this.currentIndex = this.slideCount
+						this.setTransform(-this.currentIndex * this.totalWidth)
+					}
+					this.$emit('transitionEnd', this.currentIndex - 1)
+				}, this.animDuration);
+			},
+			setTransform(position) {
+				this.swiperStyle.transform = `translate3d(${position}px, 0, 0)`
+				this.swiperStyle['webkit-transform'] = `translate3d(${position}px, 0, 0)`
+				this.swiperStyle['-ms-transform'] = `translate3d(${position}px, 0, 0)`
 			},
 			handleDom() {
 				let swiperEl = document.querySelector('.swiper')
@@ -79,12 +97,63 @@
 					this.totalWidth = swiperEl.offsetWidth
 					this.swiperStyle = swiperEl.style
 				}
-				// this.setTransform(-this.totalWidth)
+				this.setTransform(-this.totalWidth)
+			},
+			touchStart(e) {
+				if (this.scrolling) return
+				this.stopTimer()
+				this.startX = e.touches[0].pageX
+			},
+			touchMove(e) {
+				this.currentX = e.touches[0].pageX
+				this.distance = this.currentX - this.startX
+				let currentPosition = -this.currentIndex * this.totalWidth
+				let moveDistance = this.distance + currentPosition
+				this.setTransform(moveDistance)
+			},
+			touchEnd(e) {
+				let curretMove = Math.abs(this.distance)
+				if (this.distance === 0) {
+					return
+				} else if (this.distance > 0 && curretMove > this.totalWidth * this.moveRatio) {
+					this.currentIndex--
+				} else if (this.distance < 0 && curretMove > this.totalWidth * this.moveRatio) {
+					this.currentIndex++
+				}
+				this.scrollContent(-this.currentIndex * this.totalWidth)
+				this.statrTimer()
 			}
 		}
 	}
 </script>
 
 <style scoped>
-
+	#swiper {
+		overflow: hidden;
+		position: relative;
+	}
+	.swiper {
+		display: flex;
+	}
+	.indicator {
+		display: flex;
+		justify-content: center;
+		position: absolute;
+		width: 100%;
+		bottom: 8px;
+	}
+	.indi-item {
+		box-sizing: border-box;
+		width: 8px;
+		height: 8px;
+		border-radius: 4px;
+		background-color: #fff;
+		line-height: 8px;
+		text-align: center;
+		font-size: 12px;
+		margin: 0 5px;
+	}
+	.indi-item.active {
+		background-color: rgba(211, 62, 46, 1);
+	}
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,28 +1,194 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners" ref="hSwiper"></home-swiper>
+    <feature-view :features="recommends"></feature-view>
+    <recommend-view></recommend-view>
+    <tab-control :titles="['流行', '新款', '精选']"></tab-control>
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+      <li>列表31</li>
+      <li>列表32</li>
+      <li>列表33</li>
+      <li>列表34</li>
+      <li>列表35</li>
+      <li>列表36</li>
+      <li>列表37</li>
+      <li>列表38</li>
+      <li>列表39</li>
+      <li>列表40</li>
+      <li>列表41</li>
+      <li>列表42</li>
+      <li>列表43</li>
+      <li>列表44</li>
+      <li>列表45</li>
+      <li>列表46</li>
+      <li>列表47</li>
+      <li>列表48</li>
+      <li>列表49</li>
+      <li>列表50</li>
+      <li>列表51</li>
+      <li>列表52</li>
+      <li>列表53</li>
+      <li>列表54</li>
+      <li>列表55</li>
+      <li>列表56</li>
+      <li>列表57</li>
+      <li>列表58</li>
+      <li>列表59</li>
+      <li>列表60</li>
+      <li>列表61</li>
+      <li>列表62</li>
+      <li>列表63</li>
+      <li>列表64</li>
+      <li>列表65</li>
+      <li>列表66</li>
+      <li>列表67</li>
+      <li>列表68</li>
+      <li>列表69</li>
+      <li>列表70</li>
+      <li>列表71</li>
+      <li>列表72</li>
+      <li>列表73</li>
+      <li>列表74</li>
+      <li>列表75</li>
+      <li>列表76</li>
+      <li>列表77</li>
+      <li>列表78</li>
+      <li>列表79</li>
+      <li>列表80</li>
+      <li>列表81</li>
+      <li>列表82</li>
+      <li>列表83</li>
+      <li>列表84</li>
+      <li>列表85</li>
+      <li>列表86</li>
+      <li>列表87</li>
+      <li>列表88</li>
+      <li>列表89</li>
+      <li>列表90</li>
+      <li>列表91</li>
+      <li>列表92</li>
+      <li>列表93</li>
+      <li>列表94</li>
+      <li>列表95</li>
+      <li>列表96</li>
+      <li>列表97</li>
+      <li>列表98</li>
+      <li>列表99</li>
+      <li>列表100</li>
+    </ul>
   </div>
 </template>
 
 <script>
   import NavBar from 'common/navbar/NavBar'
+  import TabControl from 'content/tabControl/TabControl'
+  
+  import HomeSwiper from './childComps/HomeSwiper'
+  import FeatureView from './childComps/FeatureView'
+  import RecommendView from './childComps/RecommendView'
+
   import { getHomeMultidata } from 'network/home'
 
   export default {
     name: 'Home',
     components: {
-      NavBar
+      NavBar,
+      TabControl,
+      HomeSwiper,
+      FeatureView,
+      RecommendView
     },
     data() {
       return {
-        banners: []
+        banners: [
+          {
+            link: "https://act.mogujie.com/huanxin0001?acm=3.mce.2_10_1jhwa.43542.0.ccy5br4OlfK0Q.pos_0-m_454801-sd_119",
+            image: "https://s10.mogucdn.com/mlcdn/c45406/180926_45fkj8ifdj4l824l42dgf9hd0h495_750x390.jpg"
+          },
+          {
+            link: "https://act.mogujie.com/ruqiu00001?acm=3.mce.2_10_1ji16.43542.0.ccy5br4OlfK0R.pos_1-m_454889-sd_119",
+            image: "https://s10.mogucdn.com/mlcdn/c45406/180926_31eb9h75jc217k7iej24i2dd0jba3_750x390.jpg"
+          },
+          {
+            link: "https://act.mogujie.com/huanji001?acm=3.mce.2_10_1jfj8.43542.0.ccy5br4OlfK0S.pos_2-m_453270-sd_119",
+            image: "https://s10.mogucdn.com/mlcdn/c45406/180919_3f62ijgkj656k2lj03dh0di4iflea_750x390.jpg"
+          },
+          {
+            link: "https://act.mogujie.com/liuxing00001?acm=3.mce.2_10_1jepe.43542.0.ccy5br4OlfK0T.pos_3-m_452733-sd_119",
+            image: "https://s10.mogucdn.com/mlcdn/c45406/180917_18l981g6clk33fbl3833ja357aaa0_750x390.jpg"
+          }
+          
+        ],
+        recommends:[
+          {
+            link: "http://act.meilishuo.com/10dianlingquan?acm=3.mce.2_10_1dggc.13730.0.ccy5br4OlfK0U.pos_0-m_313898-sd_119",
+            image: "https://s10.mogucdn.com/mlcdn/c45406/180913_036dli57aah85cb82l1jj722g887g_225x225.png",
+            title: "十点抢券"
+          },
+          {
+            link: "https://act.mogujie.com/tejiazhuanmai09?acm=3.mce.2_10_1dgge.13730.0.ccy5br4OlfK0V.pos_1-m_313899-sd_119",
+            image: "https://s10.mogucdn.com/mlcdn/c45406/180913_25e804lk773hdk695c60cai492111_225x225.png",
+            title: "好物特卖"
+          },
+          {
+            link: "http://act.meilishuo.com/neigouful001?acm=3.mce.2_10_1b610.13730.0.ccy5br4OlfK0W.pos_2-m_260486-sd_119",
+            image: "https://s10.mogucdn.com/mlcdn/c45406/180913_387kgl3j21ff29lh04181iek48a6h_225x225.png",
+            title: "内购福利"
+          },
+          {
+            link: "http://act.meilishuo.com/wap/yxzc1?acm=3.mce.2_10_1dggg.13730.0.ccy5br4OlfK0X.pos_3-m_313900-sd_119",
+            image: "https://s10.mogucdn.com/mlcdn/c45406/180913_8d4e5adi8llg7c47lgh2291akiec7_225x225.png",
+            title: "初秋上新"
+          }
+        ]
       }
     },
     created() {
-      getHomeMultidata().then(res => {
-        console.log(res)
-        this.banners = res.data.banners
-      })
+      console.log('创建Home');
+      // getHomeMultidata().then(res => {
+      //   console.log(res)
+      //   this.banners = res.data.banners
+      // })
+    },
+    activated() {
+      this.$refs.hSwiper.statrTimer()
+    },
+    deactivated() {
+      this.$refs.hSwiper.stopTimer()
+    },
+    methods: {
+
     }
   }
 </script>
